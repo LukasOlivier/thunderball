9fdc4ecac8967283b48ee7d6ea06e2781a9989f0
Lukas Olivier <olivier.lukas2003@gmail.com>
2025-01-23 19:49:49 +0100

    refactor: performance improvements

--- src/components/landing/HeroBackground.svelte
+++ src/components/landing/HeroBackground.svelte
@@ -1,6 +1,7 @@
 <div class="pointer-events-none absolute inset-0 h-full w-full">
 	<enhanced:img
 		class="absolute inset-0 h-full w-full object-cover opacity-30"
+		sizes="(max-width: 600px) 480px, 1024px"
 		src="/src/lib/assets/images/hero.webp"
 		width="1024"
 		height="682"
@@ -8,9 +9,10 @@
 	/>
 	<enhanced:img
 		class="absolute inset-0 h-full w-full object-contain opacity-10"
+		sizes="(max-width: 600px) 480px, 500px"
 		src="/src/lib/assets/images/thunderball.webp"
 		width="500"
 		height="500"
-		alt="Thunderball background "
+		alt="Thunderball background"
 	/>
 </div>
