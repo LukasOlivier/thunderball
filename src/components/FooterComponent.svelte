7f140ee07ddddd1708f3dc9e8b01563df3dbc16d
Lukas Olivier <olivier.lukas2003@gmail.com>
2025-01-23 20:24:28 +0100

    feat: added credits to myself in footer

--- src/components/FooterComponent.svelte
+++ src/components/FooterComponent.svelte
@@ -1,25 +1,21 @@
 <script>
-	import { Icon } from 'svelte-icons-pack'; // Import icons from the Svelte Icons Pack
-	import { AiFillFacebook, AiFillInstagram, AiFillTwitterSquare } from 'svelte-icons-pack/ai'; // Import Font Awesome icons
+	import { Icon } from 'svelte-icons-pack';
+	import { AiFillFacebook, AiFillInstagram } from 'svelte-icons-pack/ai';
 
 	export let socials = [
 		{
 			icon: AiFillInstagram,
-			link: 'https://www.instagram.com/thunderball_2025/'
-		},
-		{
-			icon: AiFillTwitterSquare,
-			link: 'https://twitter.com/thunderball_2025'
+			link: 'https://www.instagram.com/thunderball_2025//'
 		},
 		{
 			icon: AiFillFacebook,
-			link: 'https://facebook.com/thunderball_2025'
+			link: 'https://fb.me/e/5Yy8wEytE'
 		}
 		// Add more social links as needed
 	];
 </script>
 
-<footer class="bg-primary text-secondary py-8">
+<footer class="bg-primary py-8 text-secondary">
 	<div class="container mx-auto max-w-screen-xl px-4">
 		<div class="flex flex-col items-center justify-between md:flex-row">
 			<enhanced:img
@@ -38,13 +34,19 @@
 						href={social.link}
 						target="_blank"
 						rel="noopener noreferrer"
-						class="text-secondary h text-4xl"
+						class="h text-4xl text-secondary"
 						aria-label="Social Media"
 					>
 						<Icon src={social.icon} />
 					</a>
 				{/each}
 			</div>
+		</div>
+		<div class="mt-8 border-t border-secondary/20 pt-8 text-sm text-secondary">
+			<p>
+				&copy; Website door
+				<a href="https://www.lukasolivier.be" class="cursor-pointer underline"> Lukas Olivier </a>
+			</p>
 		</div>
 	</div>
 </footer>
